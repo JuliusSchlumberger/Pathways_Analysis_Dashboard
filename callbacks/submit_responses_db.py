9e4be82c70f8a3e52ee74a05a021cf134f8eb0f3
JuliusSchlumberger <j.schlumberger@posteo.de>
2024-05-16 10:02:49 +0200

    Use PostgreSQL for storing survey responses

--- callbacks/submit_responses_db.py
+++ callbacks/submit_responses_db.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output, State
 
 
 # Replace with your actual Heroku Postgres connection URL
-DATABASE_URL = "postgres://fsyzkozjzbneio:1dfc8383fa5c7bec0dc4e2abc9c3b14a07f9e9b9376129e4bba0f67960914625@ec2-52-72-109-141.compute-1.amazonaws.com:5432/d7usfk66t0qvat"
+DATABASE_URL = "postgresql://fsyzkozjzbneio:1dfc8383fa5c7bec0dc4e2abc9c3b14a07f9e9b9376129e4bba0f67960914625@ec2-52-72-109-141.compute-1.amazonaws.com:5432/d7usfk66t0qvat"
 
 # Set up SQLAlchemy
 engine = create_engine(DATABASE_URL)
